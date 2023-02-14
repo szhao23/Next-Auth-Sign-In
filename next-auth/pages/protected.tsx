eb020f7069f4bdd38ef7037db61fc6e8dd87eb48
szhao23 <stevenzhao456@gmai.com>
2023-02-14 13:05:22 -0600

    Added Router for authentication if successfull/unsuccessful and Protected page for authenticated users

--- next-auth/pages/protected.tsx
+++ next-auth/pages/protected.tsx
@@ -1,18 +1,25 @@
 import { NextPage } from "next";
+import Router from "next/router";
 import { useSession } from "next-auth/react";
+import { useEffect } from "react";
 
 // Creating Protected Auth Routes for Successfully logged in Users
 const Protected: NextPage = (): JSX.Element => {
-  const session = useSession();
+  const { status, data } = useSession();
 
-  console.log(session);
+  useEffect(() => {
+    if (status === "unauthenticated") Router.replace("/auth/signin");
+  }, [status]);
 
-  return (
-    <div>
-      You have been successfully logged in, this page is Protected for logged in
-      users.
-    </div>
-  );
+  if (status === "authenticated")
+    return (
+      <div>
+        This page is Protected for special individuals like {"\n"}
+        {JSON.stringify(data.user, null, 2)}
+      </div>
+    );
+
+  return <div>Loading</div>;
 };
 
 export default Protected;
